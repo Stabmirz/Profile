7972512651aa3897baf79586330bb3d0cbac3fe9
Stabmirz <amirza@techlaunch.io>
2019-01-16 21:43:01 -0500

    update

--- index.php
+++ index.php
@@ -2,26 +2,26 @@
 
 <center><h1>Enter Your Informations</h1></center>
 <br><br>
-   <form action="profile.php" method="post" enctype="multipart/form-data">
-        <h5>Name: </h5><input type="text" name="name" required/>
-        <br><br>
-        <h5>Date of Birth: </h5><input type="date" name="date" required/>
-        <br><br>
-        <h5>Short Biography: </h5><textarea name="biography" rows="5" cols="40" required></textarea>
-        <br><br>
-        <h5>list of interests:</h5>
-        <select name = 'subject[]' multiple size = 5 required>
-        <option value="Playing">Playing</option>
-        <option value="Reading">Reading</option>
-        <option value="Watching TV">Watching TV</option>
-        <option value="Singing">Singing</option>
-        <option value="Coading">Coading</option>
-        <option value="Fishing">Fishing</option>
-        </select><br><br>
-        Upload Image<br><input type="file" name="fileToUpload" required><br><br>
-       <input type = "submit" name = "submit" value = Submit>
-   </form>
-   <br>
-   <br>
-   <br>
-   <?php include 'partials/footer.php';?>
+ <form action="profile.php" method="post" enctype="multipart/form-data">
+      <h5>Name: </h5><input type="text" name="name" required/>
+      <br><br>
+      <h5>Date of Birth: </h5><input type="date" name="date" required/>
+      <br><br>
+      <h5>Short Biography: </h5><textarea name="biography" rows="5" cols="40" required></textarea>
+      <br><br>
+      <h5>list of interests:</h5>
+      <select name = 'subject[]' multiple size = 5 required>
+      <option value="Playing">Playing</option>
+      <option value="Reading">Reading</option>
+      <option value="Watching TV">Watching TV</option>
+      <option value="Singing">Singing</option>
+      <option value="Coading">Coading</option>
+      <option value="Fishing">Fishing</option>
+      </select><br><br>
+      <h5>Upload Image :<br><br><input type="file" name="upload" required></h5><br><br>
+     <input type = "submit" name = "submit" value = Submit>
+ </form>
+ <br>
+ <br>
+ <br>
+ <?php include 'partials/footer.php';?>
--- partials/footer.php
+++ partials/footer.php
@@ -1,4 +1,3 @@
-<br>
                 <div class="card text-center">
                     <div class="card-footer text-muted">
                         <div class="p-3 mb-2 bg-info text-dark">
